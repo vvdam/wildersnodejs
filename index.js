ef6380b3a84cf092b8913dcf76c520b9aff69163
vvdam <maaalouladam@gmail.com>
2024-03-19 10:58:09 +0100

    ouai ouai

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+console.log(
+    chalk.magentaBright(
+        "(Ouais cousin) J'suis dans l'bâtiment, je dirais même j'suis dans la cuisine (oof) Mon grand frère est à l'hopital parce que dans son café j'ai mis de la lean Jme suis fait monter en l'air en Belgique parce que j'ai filmé les meufs des vitrines (aaah) Nique les keufs, nique les porcs (aaah), au collège les meufs disaient que j'étais un porc (oof) Je mange beaucoup c'est pour ça que j'ai un gros corps Je vais tous les fumer, la concurrence dans le micro-onde, le mec de ma meuf s'habille au marché, il a même pas honte"
+    )
+); //
