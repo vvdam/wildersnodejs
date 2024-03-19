7b9c69dae844811735a51351245f662d23495518
vvdam <maaalouladam@gmail.com>
2024-03-19 11:02:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import chalk from "chalk";
 
 console.log(
     chalk.magentaBright(
-        "(Ouais cousin) J'suis dans l'bâtiment, je dirais même j'suis dans la cuisine (oof) Mon grand frère est à l'hopital parce que dans son café j'ai mis de la lean Jme suis fait monter en l'air en Belgique parce que j'ai filmé les meufs des vitrines (aaah) Nique les keufs, nique les porcs (aaah), au collège les meufs disaient que j'étais un porc (oof) Je mange beaucoup c'est pour ça que j'ai un gros corps Je vais tous les fumer, la concurrence dans le micro-onde, le mec de ma meuf s'habille au marché, il a même pas honte"
+        "Ooorh, j'ai dit pute ! Huit morts... Kaarism Allez Paris Six blessés, ma lubellule ! La place de la coupe, c'est où ? Au PSG ! J'vais cogner mon fils s'il devient pas comme Mbappé (oof) Müller, sale raciste, méga-nazi, on va te baffer (oof) Nous on cherche des streams, jamais de la vie on paiera RMC J'ai frappé ma meuf parce qu'elle était pour le Bayern de Munich"
     )
 ); //
